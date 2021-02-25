dd6a5d8fc22137ebf9673629efc176c761174e9b
cSDes1gn <christian911@sympatico.ca>
2021-02-25 16:40:15 -0500

    updates

--- docker/build.sh
+++ docker/build.sh
@@ -6,13 +6,13 @@ set -e
 source .env
 
 # handle all non-zero exit status codes with a slack notification
-trap 'handler $? $LINENO' EXIT
+trap 'handler $?' EXIT
 
 handler () {
     if [ "$1" != "0" ]; then
         printf "%b" "${OKB}Notifying slack channel of snap build failure.${NC}\n"
         curl -X POST -H 'Content-type: application/json' \
-            --data "{\"text\":\"Snap build server job failed with exit status: $1 on line $2\"}" https://hooks.slack.com/services/"$SLACK_IDENTIFIER"
+            --data "{\"text\":\"Snap deploy server job failed with exit status: $1\"}" https://hooks.slack.com/services/"$SLACK_IDENTIFIER"
         printf "%b" "${OKG} ✓ ${NC}complete\n"
     fi
 }
@@ -28,5 +28,5 @@ snapcraft login --with .snapcraft
 # Notify slack channel of build success
 printf "%b" "${OKB}Notifying slack channel of snap build success.${NC}"
 curl -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"Snap build server job complete. Download and install the snap file and the build logs here: https://s3.console.aws.amazon.com/s3/buckets/snapbuilds?region=ca-central-1&tab=objects\"}" https://hooks.slack.com/services/"$SLACK_IDENTIFIER"
+    --data "{\"text\":\"Snap deployment complete. View the latest snap release: https://snapcraft.io/iris-incuvers/releases\"}" https://hooks.slack.com/services/"$SLACK_IDENTIFIER"
 printf "%b" "${OKG} ✓ ${NC}complete"
