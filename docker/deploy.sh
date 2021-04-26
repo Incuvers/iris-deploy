dce1de38da2f2204f00e33dee40f2743886e40b5
cSDes1gn <christian911@sympatico.ca>
2021-04-26 17:10:52 -0400

    specify target file using var

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -50,7 +50,7 @@ printf "%b" "${OKG} ✓ ${NC}complete\n"
 # login to snapcraft and release to edge
 printf "%b" "${OKB}Publishing ${OBJECT} to ${RELEASE_CHANNEL}${NC}\n"
 echo "${INPUT_SNAP_TOKEN}" | snapcraft login --with -
-snapcraft upload ./*.snap --release="${RELEASE_CHANNEL}"
+snapcraft upload "$OBJECT" --release="${RELEASE_CHANNEL}"
 printf "%b" "${OKG} ✓ ${NC}complete\n"
 
 # Notify slack channel of build success
