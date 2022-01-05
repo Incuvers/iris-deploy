50ffd3f9c2dc0d45fed6ebc980e79724d94639ce
cSDes1gn <christian911@sympatico.ca>
2022-01-05 10:35:35 -0500

    fix pathing

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -50,7 +50,7 @@ printf "%b" "${OKG} ✓ ${NC}complete\n"
 # login to snapcraft and release to edge
 echo $(which snapcraft)
 python3 -m pip list
-./snap/bin/snapcraft --help
+/snap/bin/snapcraft --help
 printf "%b" "${OKB}Publishing ${OBJECT} to ${RELEASE_CHANNEL}${NC}\n"
 echo "${INPUT_SNAP_TOKEN}" | snapcraft login --with -
 snapcraft upload "$OBJECT" --release="${RELEASE_CHANNEL}"
