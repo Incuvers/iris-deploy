af2732fb6b89cb01927b0f0cd258d696f5f7fdb7
cSDes1gn <christian911@sympatico.ca>
2022-01-05 11:33:13 -0500

    updates

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -24,10 +24,12 @@ SHELL="/bin/bash"
 # Required for aws s3 push script
 BUCKET="snapbuilds"
 OBJECT="iris-incuvers-prod.snap"
-
 # Snap release channel
 RELEASE_CHANNEL="edge"
 
+# ensure snapcraft binary is discoverable
+export PATH="/snap/bin:$PATH"
+echo "$PATH"
 # handle all non-zero exit status codes with a slack notification
 trap 'handler $?' EXIT
 
@@ -46,11 +48,10 @@ printf "%b" "${OKB}Pulling $OBJECT to S3 bucket $BUCKET as $OBJECT${NC}\n"
 printf "%b" "${OKG} ✓ ${NC}complete\n"
 
 # TODO: Add checksum cross-validation
-export SNAP_VERSION=3.8
 # login to snapcraft and release to edge
 printf "%b" "${OKB}Publishing ${OBJECT} to ${RELEASE_CHANNEL}${NC}\n"
-echo "${INPUT_SNAP_TOKEN}" | python3 -m snapcraft.cli login --with -
-python3 -m snapcraft.cli upload "$OBJECT" --release="${RELEASE_CHANNEL}"
+echo "${INPUT_SNAP_TOKEN}" | snapcraft login --with -
+snapcraft upload "$OBJECT" --release="${RELEASE_CHANNEL}"
 printf "%b" "${OKG} ✓ ${NC}complete\n"
 
 # Notify slack channel of build success
