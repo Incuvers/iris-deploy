0f478e572939cd88b83c7b466d9d71967fa05fee
cSDes1gn <christian911@sympatico.ca>
2022-01-05 10:48:15 -0500

    add snap version export

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -46,7 +46,7 @@ printf "%b" "${OKB}Pulling $OBJECT to S3 bucket $BUCKET as $OBJECT${NC}\n"
 printf "%b" "${OKG} ✓ ${NC}complete\n"
 
 # TODO: Add checksum cross-validation
-
+export SNAP_VERSION=3.8
 # login to snapcraft and release to edge
 printf "%b" "${OKB}Publishing ${OBJECT} to ${RELEASE_CHANNEL}${NC}\n"
 echo "${INPUT_SNAP_TOKEN}" | python3 -m snapcraft.cli login --with -
